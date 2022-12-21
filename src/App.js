57a61406a70f4a59b820a87c5f8b9f58707e8ea1
BrightTechnologies1 <silascyrax@gmail.com>
2022-12-21 18:33:18 +0100

    feat: Added Clock

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Clock from './components/Clock';
 import DrumKit from './components/DrumKit';
 
 function App() {
   return (
     <div className="">
       <DrumKit />
+      <Clock />
     </div>
   );
 }
---
+++ src/components/Clock/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+function Clock() {
+
+    const secondHand = document.querySelector('.seconds-hand')
+    const minutesHand = document.querySelector('.minutes-hand')
+    const hoursHand = document.querySelector('.hours-hand')
+
+    function setDate() {
+        const now = new Date()
+        //Sec. hand
+        const seconds = now.getSeconds()
+        const secondsDegrees = ((seconds / 60) * 360) + 90
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+        //Min. hand
+        const minutes = now.getMinutes()
+        const minutesDegrees = ((minutes / 60) * 360) + 90
+        minutesHand.style.transform = `rotate(${minutesDegrees}deg)`
+        // Hour hand
+        const hours = now.getMinutes()
+        const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90
+        hoursHand.style.transform = `rotate(${hoursDegrees}deg)`
+
+        console.log(seconds)
+    }
+
+    setInterval(setDate, 1000)
+
+  return (
+    <div>
+        <div className='clock-bg'>
+            <div>
+                <div className="clock">
+                    <div className="clock-inner">
+                        <div className="hand hours-hand"></div>
+                        <div className="hand minutes-hand"></div>
+                        <div className="hand seconds-hand"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Clock
